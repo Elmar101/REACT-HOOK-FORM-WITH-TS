15fa99af53cd2892f389b1e93c8d388bcf95c77e
elmar.amanov <elmar.amanov@cybernet.az>
2023-09-14 20:36:52 +0400

    USE ERROR MESSAGE 'required and pattern'

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { RHFYouTubeForm } from "./components/RHFYouTubeForm";
+
+function App() {
+  return (
+    <div>
+      <RHFYouTubeForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RHFYouTubeForm.tsx
@@ -0,0 +1,67 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+let count = 0;
+
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
+export const RHFYouTubeForm = () => {
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const onSubmit = (data: FormValues) => {
+    console.log("Form submitted", data);
+  };
+
+  count++;
+  return (
+    <div>
+      <h1>YouTube Form ({count})</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: { value: true, message: "Username is required" },
+          })}
+        />
+        <p className="error">{errors.username?.message}</p>
+
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
+        <p className="error">{errors.email?.message}</p>
+
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            required: { value: true, message: "Channel is required" },
+          })}
+        />
+        <p className="error">{errors.channel?.message}</p>
+
+        <button>Submit</button>
+      </form>
+
+      <DevTool control={control} />
+    </div>
+  );
+};
